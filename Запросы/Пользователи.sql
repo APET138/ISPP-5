9967907c7fbabd2559c4f10c83fce326ef9f1a14
Pleakses <151109447+Pleakses@users.noreply.github.com>
2023-12-06 21:21:00 +0800

    Delete Запросы directory

--- Запросы/Пользователи.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE Пользователи
-(	[Код пользователя] int IDENTITY (1,1) NOT NULL,
-		constraint PK6 primary key ([Код пользователя]),
-	Фамилия	varchar(50),
-	Имя		varchar(50),
-	Отчество			varchar(50),
-	Логин		varchar(16) not null,
-	Пароль			varchar(16) not null,
-	[Права доступа]	varchar(50) default 'Гость')
