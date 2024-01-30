47ccf6e18a10d33a8aa770c9159518d8bc499cc9
Швидко Иван <151105537+IvanShvidko@users.noreply.github.com>
2024-01-30 22:52:06 +0800

    Add files via upload

--- Практическая работа 2/PR2/src/Main.java
+++ Практическая работа 2/PR2/src/Main.java
@@ -1,21 +1,21 @@
 import java.util.Scanner;
-public class Main
-{
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите количество баллов: ");
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите целое число N: ");
+        int N = scanner.nextInt();
 
-        if (a<=25)
-            System.out.println("Несдача");
-
-        if((a<=50)&&(a>25))
-            System.out.println("Удовлетворительно");
-
-        if((a<=75)&&(a>50))
-            System.out.println("Хорошо");
-
-        if((a<=100)&&(a>75))
-            System.out.println("Отлично!");
+        for (int i = 1; i <= N; i++) {
+            for (int k = N; k >= i; k--) {
+                System.out.print("  ");
+            }
+            for (int j = i; j >= 1; j--) {
+                System.out.print(j + " ");
+            }
+            for (int j = 2; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
     }
 }
